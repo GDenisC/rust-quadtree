ab6063fb37444ee0f42c2482e69a19700658ae46
DenisC <denis0ches00@gmail.com>
2023-12-23 12:52:32 +0700

    types support

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import { QuadTree as RSQuadTree, AABB, XY } from './quadtree';
+
+class Point extends XY {
+    constructor(public x: number, public y: number);
+    static from(xy: XY): Point;
+    toString(): string;
+}
+
+class Rect extends AABB {
+    constructor(x1: number, y1: number, x2: number, y2: number);
+}
+
+class Circle {
+    constructor(public x: number, public y: number, public radius: number);
+    get aabb(): Rect;
+}
+
+class QuadTree extends RSQuadTree {
+    constructor(bound: [number, number]);
+    query(aabb: Rect | Circle): Point[];
+    get points(): Point[];
+}
+
+export { QuadTree, XY, AABB }
